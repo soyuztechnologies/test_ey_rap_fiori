061e8ca3a92fae70cf71a4b1b1d3316bfef9f786
Anubhav Oberoy <anubhav.abap@gmail.com>
2024-07-03 07:01:23 +0000

    My Stable app

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("ats.ao.travelproc.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
